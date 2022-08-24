a57286b62d0393b9a03a6374515a7c05481f58b2
tdpham <thangpham2024@u.northwestern.edu>
2022-08-24 09:16:49 -0500

    change option for input geometry

--- create_g16_input_geometry.py
+++ create_g16_input_geometry.py
@@ -4,19 +4,27 @@ import sys
 
 """ Objectives: Create Gaussian 16 input geometries (without using Gaussview)"""
 # Output file name
-g16_input = sys.argv[1]
-#options = ["output_xyz", "output_movie", "freeze_atoms"]
+option = int(sys.argv[1])
+g16_input = sys.argv[2]
 
-#option = "output_movie"
-#option = "output_xyz"
-option = "freeze_atoms"
-#option = "label_fragment"
+# Options:
+# 1: Create output movie from Gaussian 16 output
+# 2: Create the geometry of the last step from Gaussian 16 output
+# 3: Freeze specific atom index
+# 4: Add fragment labels
+
 # Atomic numbers
 ptable = {'X': 0, 'H': 1, 'He': 2, 'Li': 3, 'Be': 4, 'B': 5, 'C': 6, 'N': 7, 'O': 8, 'F': 9, 'Ne': 10, 'Na': 11, 'Mg': 12, 'Al': 13, 'Si': 14, 'P': 15, 'S': 16, 'Cl': 17, 'Ar': 18, 'K': 19, 'Ca': 20, 'Sc': 21, 'Ti': 22, 'V': 23, 'Cr': 24, 'Mn': 25, 'Fe': 26, 'Co': 27, 'Ni': 28, 'Cu': 29, 'Zn': 30, 'Ga': 31, 'Ge': 32, 'As': 33, 'Se': 34, 'Br': 35, 'Kr': 36, 'Rb': 37, 'Sr': 38, 'Y': 39, 'Zr': 40, 'Nb': 41, 'Mo': 42, 'Tc': 43, 'Ru': 44, 'Rh': 45, 'Pd': 46, 'Ag': 47, 'Cd': 48, 'In': 49, 'Sn': 50, 'Sb': 51, 'Te': 52, 'I': 53, 'Xe': 54, 'Cs': 55, 'Ba': 56, 'La': 57, 'Ce': 58, 'Pr': 59, 'Nd': 60, 'Pm': 61, 'Sm': 62, 'Eu': 63, 'Gd': 64, 'Tb': 65, 'Dy': 66, 'Ho': 67, 'Er': 68, 'Tm': 69, 'Yb': 70, 'Lu': 71, 'Hf': 72, 'Ta': 73, 'W': 74, 'Re': 75, 'Os': 76, 'Ir': 77, 'Pt': 78, 'Au': 79, 'Hg': 80, 'Tl': 81, 'Pb': 82, 'Bi': 83, 'Po': 84, 'At': 85, 'Rn': 86, 'Fr': 87, 'Ra': 88, 'Ac': 89, 'Th': 90, 'Pa': 91, 'U': 92, 'Np': 93, 'Pu': 94, 'Am': 95, 'Cm': 96, 'Bk': 97, 'Cf': 98, 'Es': 99, 'Fm': 100, 'Md': 101, 'No': 102, 'Lr': 103, 'Rf': 104, 'Db': 105, 'Sg': 106, 'Bh': 107, 'Hs': 108, 'Mt': 109, 'Ds': 110, 'Rg': 111, 'Cn': 112, 'Nh': 113, 'Fl': 114, 'Mc': 115, 'Lv': 116, 'Ts': 117, 'Og': 118}
 
 def isfloat(value):
     try:
         float(value)
+        return True
+    except ValueError:
+        return False
+def istint(value):
+    try:
+        int(value)
         return True
     except ValueError:
         return False
@@ -30,13 +38,12 @@ def iscoord(line):
     else:
         return False
 
-if option == "output_xyz":
+if option == 1:
     # Get output geometry from awk 
     start = int(subprocess.check_output("awk '/Input/{{print NR}}' {} | tail -1".format(g16_input), shell=True).decode('ascii'))
     #start = subprocess.check_output("awk '/Input/{{print NR}}' {} | tail -1".format(g16_input), shell=True).decode('ascii')
     end = int(subprocess.check_output("awk '/Rotational/{{print NR}}' {} | tail -1".format(g16_input), shell=True).decode('ascii'))
     geometry_output = subprocess.check_output("awk 'NR>={} && NR<={} {{print}}' {}".format(start+5, end-2,g16_input), shell=True).decode('ascii')
-
 
     geometry_output = filter(None, geometry_output.split('\n'))
     output = []
@@ -54,12 +61,9 @@ if option == "output_xyz":
         print(' '.join(line))
     f.close()
 
-if option == "output_movie":
+if option == 2:
     start = subprocess.check_output("awk '/Input/{{print NR}}' {}".format(g16_input), shell=True).decode('ascii')
     end = subprocess.check_output("awk '/Rotational/{{print NR}}' {}".format(g16_input), shell=True).decode('ascii')
-
-    print(start)
-    print(end)
     starts = filter(None, str(start).split('\n'))
     ends = filter(None, str(end).split('\n'))
 
@@ -87,9 +91,7 @@ if option == "output_movie":
         f.close()
 
 # Atom indices from ase gui (atom index counts from 0)
-
-if option == "freeze_atoms":
-
+if option == 3:
     data = []
     with open(g16_input, "r") as f:
         for line in f:
@@ -111,7 +113,7 @@ if option == "freeze_atoms":
                 f.write(str(item) + ' ')
             f.write("\n")
 
-if option == "label_fragment":
+if option == 4:
     data = []
     with open(g16_input, "r") as f:
         for line in f:
